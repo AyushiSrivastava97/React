f67b2c453c533612d25af33f1dd443102ef70c39
AyushiSrivastava97 <40993320+AyushiSrivastava97@users.noreply.github.com>
2019-12-06 11:24:36 +0530

    Add files via upload

---
+++ brand.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Brand({brand}){
+    return(
+        <div>
+        <h2>{Brand.name} {Brand.price} {Brand.size}</h2>
+        </div>
+    )
+}
+
+export default Brand
